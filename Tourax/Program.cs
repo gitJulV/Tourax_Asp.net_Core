e0fc10e736f39a205c18c55b226e658205b459ca
AXOCAMP105 <AXOCAMP105@AXOCAMP05>
2021-06-22 16:24:09 +0200

    modification suite capture d'écran

--- Tourax/Program.cs
+++ Tourax/Program.cs
@@ -14,24 +14,6 @@ namespace Tourax
 {
     public class Program
     {
-        private static void CreateDbIfNotExists(IHost host)
-        {
-            using(var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<TouraxDbContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch(Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred creating the DB.");
-                }
-            }
-        }
-
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
@@ -40,12 +22,28 @@ namespace Tourax
 
             host.Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<TouraxDbContext>();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred creating the DB.");
+            }
+        }
     }
 }
